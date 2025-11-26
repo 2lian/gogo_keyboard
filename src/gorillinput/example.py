1f88733406f24b450f4c9c71939d9e503f793e78
Neppel Elian <elian.neppel@posteo.eu>
2025-11-27 02:46:58 +0900

    and it works

---
+++ src/gorillinput/example.py
@@ -0,0 +1,58 @@
+import asyncio
+
+import sdl2
+import sdl2.ext
+
+from . import codes
+from .keyboard import Key, KeySub
+
+
+async def detect_window_closed(event: asyncio.Event):
+    await event.wait()
+    print("Gorilla window closed by user.")
+
+
+async def detect_CtrlC(key_sub: KeySub):
+    async for k in key_sub.listen_reliable():
+        if k.symbol == "C" and (k.modifiers & codes.MODIFIER_LCTRL) and k.is_pressed:
+            print("Ctrl+C in Gorilla window by user.")
+            return
+
+
+async def print_keys(key_sub: KeySub):
+    async for k in key_sub.listen_reliable():
+        print(k)
+
+
+async def async_main():
+    window_closed_event = asyncio.Event()
+    key_sub = KeySub(
+        termination_callback=window_closed_event.set,
+    )
+
+    try:
+        print_task = asyncio.create_task(print_keys(key_sub))
+        close_task = asyncio.create_task(detect_window_closed(window_closed_event))
+        ctrl_task = asyncio.create_task(detect_CtrlC(key_sub))
+        await asyncio.wait(
+            [print_task, close_task, ctrl_task], return_when=asyncio.FIRST_COMPLETED
+        )
+        print_task.cancel()
+        close_task.cancel()
+        ctrl_task.cancel()
+    finally:
+        key_sub.close()
+
+
+def main():
+    try:
+        asyncio.run(async_main())
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt in python process")
+    finally:
+        sdl2.ext.quit()
+    print("Exited cleanly :)")
+
+
+if __name__ == "__main__":
+    main()
